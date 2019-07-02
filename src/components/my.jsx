bd232811d78f146b9174d99c60c403ff646b6796
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2019-07-02 16:41:06 +0700

    :sparkles: Add link to blog and illustration

--- src/components/my.jsx
+++ src/components/my.jsx
@@ -3,6 +3,7 @@ import { Link } from 'gatsby';
 
 import mathematicsSvg from '../images/undraw_mathematics_4otb.svg';
 import startupLifeSvg from '../images/undraw_startup_life_2du2.svg';
+import contentCreatorSvg from '../images/undraw_content_creator_xeju.svg';
 
 const My = () => (
   <section className="section is-family-code">
@@ -11,11 +12,15 @@ const My = () => (
       <div className="columns">
         <Link style={{ backgroundColor: '#3F51B5' }} className="column is-4 has-text-centered hv-section-card" to="/about">
           <h3 className="title hv-animate-text-stroke">Self</h3>
-          <img src={mathematicsSvg} alt="Portfolio" />
+          <img src={mathematicsSvg} alt="About me" />
         </Link>
         <Link style={{ backgroundColor: '#673AB7' }} className="column is-4 has-text-centered hv-section-card" to="/portfolio">
           <h3 className="title hv-animate-text-stroke">Portfolio</h3>
           <img src={startupLifeSvg} alt="Portfolio" />
+        </Link>
+        <Link style={{ backgroundColor: '#009688' }} className="column is-4 has-text-centered hv-section-card" to="/blog">
+          <h3 className="title hv-animate-text-stroke">Blog</h3>
+          <img src={contentCreatorSvg} alt="Blog" />
         </Link>
       </div>
     </div>
