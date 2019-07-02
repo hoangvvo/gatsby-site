222713729d497b387345690528a4a7b918ad578f
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2019-07-03 04:40:53 +0700

    :lipstick: Add multiline to column

--- src/components/my.jsx
+++ src/components/my.jsx
@@ -9,16 +9,16 @@ const My = () => (
   <section className="section is-family-code">
     <div className="container">
       <h3 className="title hv-section-title hv-animate-text-stroke">My...</h3>
-      <div className="columns">
-        <Link style={{ backgroundColor: '#3F51B5' }} className="column is-4 has-text-centered hv-section-card" to="/about">
+      <div className="columns is-multiline">
+        <Link style={{ backgroundColor: '#3F51B5' }} className="column has-text-centered hv-section-card" to="/about">
           <h3 className="title hv-animate-text-stroke">Self</h3>
           <img src={mathematicsSvg} alt="About me" />
         </Link>
-        <Link style={{ backgroundColor: '#673AB7' }} className="column is-4 has-text-centered hv-section-card" to="/portfolio">
+        <Link style={{ backgroundColor: '#673AB7' }} className="column has-text-centered hv-section-card" to="/portfolio">
           <h3 className="title hv-animate-text-stroke">Portfolio</h3>
           <img src={startupLifeSvg} alt="Portfolio" />
         </Link>
-        <Link style={{ backgroundColor: '#009688' }} className="column is-4 has-text-centered hv-section-card" to="/blog">
+        <Link style={{ backgroundColor: '#009688' }} className="column has-text-centered hv-section-card" to="/blog">
           <h3 className="title hv-animate-text-stroke">Blog</h3>
           <img src={contentCreatorSvg} alt="Blog" />
         </Link>
