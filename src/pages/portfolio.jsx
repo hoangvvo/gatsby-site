d8af539d4756d826945c4617f5f0b8bdf6432485
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2019-07-04 10:42:51 +0700

    :zap: Add Image Fluid

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,41 +1,55 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-
+import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 import PortfolioItem from '../templates/portfolio-item';
 import My from '../components/my';
 
-import luvScriptStudioSquare from '../images/luvscriptstudio_square.jpg';
-
-const PortfolioPage = () => (
-  <Layout>
-    <SEO title="Portfolio" description="I have been enjoying working on different projects during the past years to polish my skills in different fields. Check out my work!" />
-    <section className="section hv-mt-3">
-      <div className="container">
-        <h1 className="title hv-section-title hv-animate-text-stroke">Portfolio</h1>
-        <p className="hv-section-subtitle-hashtags">
-          <span>#project</span>
+const PortfolioPage = ({ data }) => {
+  const { luvScriptStudioImage } = data;
+  return (
+    <Layout>
+      <SEO title="Portfolio" description="I have been enjoying working on different projects during the past years to polish my skills in different fields. Check out my work!" />
+      <section className="section hv-mt-3">
+        <div className="container">
+          <h1 className="title hv-section-title hv-animate-text-stroke">Portfolio</h1>
+          <p className="hv-section-subtitle-hashtags">
+            <span>#project</span>
           ,
-          {' '}
-          <span>
+            {' '}
+            <span>
             #creative
-            <span role="img" aria-label="Star">⭐</span>
-          </span>
+              <span role="img" aria-label="Star">⭐</span>
+            </span>
           ,
-          {' '}
-          <span>
+            {' '}
+            <span>
             #lit
-            <span role="img" aria-label="Fire">🔥</span>
-          </span>
-        </p>
-        <div className="hv-section-portfolio hv-mt-3">
-          <PortfolioItem name="LuvScript Studio" color="#ba194d" image={luvScriptStudioSquare} link="https://www.facebook.com/luvscript.studio/" linkTitle="Go and love" description="Determined to create helpful tiny bits to tackle the biggest complications. For the community. For ever." />
+              <span role="img" aria-label="Fire">🔥</span>
+            </span>
+          </p>
+          <div className="hv-section-portfolio hv-mt-3">
+            <PortfolioItem name="LuvScript Studio" color="#ba194d" imageFluid={luvScriptStudioImage.childImageSharp.fluid} link="https://www.facebook.com/luvscript.studio/" linkTitle="Go and love" description="Determined to create helpful tiny bits to tackle the biggest complications. For the community. For ever." />
+          </div>
         </div>
-      </div>
-    </section>
-    <My />
-  </Layout>
-);
+      </section>
+      <My />
+    </Layout>
+  );
+};
 
 export default PortfolioPage;
+
+export const query = graphql`
+  query PortfolioPageQuery {
+    luvScriptStudioImage: file(relativePath: { eq: "luvscriptstudio_square.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`;
--- src/templates/portfolio-item.jsx
+++ src/templates/portfolio-item.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import Img from 'gatsby-image';
 import PropTypes from 'prop-types';
 
 const PortfolioItem = ({
-  name, description, color, image, link, linkTitle,
+  name, description, color, link, linkTitle, imageFluid, image,
 }) => (
   <div className="columns hv-section-portfolio-item">
     <div className="column is-4">
-      <img src={image} alt={name} />
+      { (imageFluid !== undefined)
+        ? (<Img fluid={imageFluid} alt={name} />)
+        : (<img src={image} alt={name} />) }
     </div>
     <div className="column is-8 content">
       <h4 className="title hv-portfolio-title is-1" style={{ color }}>{name}</h4>
@@ -22,12 +25,15 @@ PortfolioItem.defaultProps = {
   color: '',
   link: '#',
   linkTitle: PortfolioItem.name,
+  imageFluid: undefined,
+
 };
 
 PortfolioItem.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string,
   image: PropTypes.string,
+  imageFluid: PropTypes.shape(),
   color: PropTypes.string,
   link: PropTypes.string,
   linkTitle: PropTypes.string,
