b24e85ce0f4d54c511712771f5040017bda76114
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-01-20 18:01:39 -0500

    Add Come Find Me Section in homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import IconGithub from '../assets/svgs/github.svg';
+import IconInstagram from '../assets/svgs/instagram.svg';
+import IconTwitter from '../assets/svgs/twitter.svg';
 
 const IndexPage = () => (
   <Layout>
@@ -13,31 +16,18 @@ const IndexPage = () => (
         Hi, I&apos;m <b className="font-black">Hoang</b>
       </h1>
       <p className="text-5xl font-hairline leading-tight opacity-75 hover:opacity-100 transition-opacity duration-100 ease-out">
-        I am a{' '}
-        <b className="font-black underline-bg theme-gradient">
-          curious learner
-        </b>
-        , a{' '}
-        <b className="font-black underline-bg theme-gradient">
-          practical thinker
-        </b>
-        , a{' '}
-        <b className="font-black underline-bg theme-gradient">
-          tech-savvy person
-        </b>
-        , and{' '}
-        <b className="font-black underline-bg theme-gradient">
-          your good friend
-        </b>
-        .
+        I am a <b className="font-black">curious learner</b>, a{' '}
+        <b className="font-black">practical thinker</b>, a{' '}
+        <b className="font-black">tech-savvy person</b>, and{' '}
+        <b className="font-black">your good friend</b>.
       </p>
     </section>
-    <section className="container mx-auto leading-relaxed py-32">
+    <section className="container mx-auto leading-relaxed py-16">
       <h2 className="text-7xl font-extrabold text-stroke text-center">
         I do quite lots of things
       </h2>
       <p className="opacity-75 hover:opacity-100 transition-opacity duration-100 ease-out text-2xl">
-        I am simply an average guy who does <b>Computer Programming</b>,{' '}
+        I am an average guy who does <b>Computer Programming</b>,{' '}
         <b>Graphic Design</b>, <b>Photography</b>, and a bunch of other wacky
         things.
       </p>
@@ -50,10 +40,9 @@ const IndexPage = () => (
         The Web
       </h3>
       <p className="mt-2 opacity-75 hover:opacity-100 transition-opacity duration-100 ease-out text-2xl">
-        I am a <b>Javascript</b> developer comfortable with <b>React</b>{' '}
-        frameworks such as
-        <b>Gatsby.js</b> and <b>Next.js</b>. At the same time, I am building
-        apps with <b>Node.js</b> frameworks and powering APIs with{' '}
+        I am a <b>Javascript</b> developer, comfortable with <b>React</b>{' '}
+        frameworks such as <b>Gatsby.js</b> and <b>Next.js</b>. At the same
+        time, I am building apps with <b>Node.js</b> and powering APIs with{' '}
         <b>GraphQL</b>.
       </p>
       <p className="mt-4 opacity-75 hover:opacity-100 transition-opacity duration-100 ease-out text-2xl">
@@ -78,13 +67,48 @@ const IndexPage = () => (
         The Art
       </h3>
       <p className="mt-2 opacity-75 hover:opacity-100 transition-opacity duration-100 ease-out text-2xl">
-        I am an enthusiastic photographer, loyal to the Sony ecosystem. I do
-        portrait and landscape and also play around with photo manipulation.
+        I am an enthusiastic photographer, loyal to the Sony ecosystem. I mostly{' '}
+        do portrait and landscap, but I also play around with photo
+        manipulation.
       </p>
       <p className="mt-2 opacity-75 hover:opacity-100 transition-opacity duration-100 ease-out text-2xl">
         Back in the day, I was a graphic designer for local organizations. I
         also do digital music composition from time to time using FL Studio.
       </p>
+    </section>
+    <section className="container mx-auto leading-relaxed py-16">
+      <h2 className="text-6xl font-extrabold text-stroke text-center">
+        Come find me
+      </h2>
+      <div className="flex justify-center mt-2">
+        <a
+          href="https://www.instagram.com/hoangvvo/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center rounded-full mx-1 py-4 px-8 opacity-75 hover:opacity-100 text-accents-700 shadow-md hover:shadow-lg transform hover:translate-y-1 transition-all ease-in-out duration-300"
+        >
+          <IconInstagram className="mr-2" />
+          Instagram
+        </a>
+        <a
+          href="https://twitter.com/hoangvvo"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center rounded-full mx-1 py-4 px-8 opacity-75 hover:opacity-100 shadow-md hover:shadow-lg transform hover:translate-y-1 transition-all ease-in-out duration-300"
+        >
+          <IconTwitter className="mr-2" />
+          Twitter
+        </a>
+        <a
+          href="https://github.com/hoangvvo"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center rounded-full mx-1 py-4 px-8 opacity-75 hover:opacity-100 shadow-md hover:shadow-lg transform hover:translate-y-1 transition-all ease-in-out duration-300"
+        >
+          <IconGithub className="mr-2" />
+          Github
+        </a>
+      </div>
     </section>
   </Layout>
 );
