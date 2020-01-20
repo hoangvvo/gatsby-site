8d98815828c3bf4edd5c08ea3fbe4b2ba782853a
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-01-20 18:48:03 -0500

    Redesign Portfolio page

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -4,7 +4,6 @@ import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import My from '../components/my';
 
 const PortfolioPage = ({ data }) => {
   const portfolioItems = data.allContentfulPortfolio.edges;
@@ -14,62 +13,41 @@ const PortfolioPage = ({ data }) => {
         title="Portfolio"
         description="I have been enjoying working on different projects during the past years to polish my skills in different fields. Check out my work!"
       />
-      <section className="section hv-mt-3">
-        <div className="container">
-          <h1 className="title hv-section-title hv-animate-text-stroke">
-            Portfolio
-          </h1>
-          <p className="hv-section-subtitle-hashtags">
-            <span>#project</span>
-            ,
-            {' '}
-            <span>
-              #creative
-              <span role="img" aria-label="Star">
-                ⭐
+
+      <div className="container mx-auto px-2 py-32">
+        <h1 className="text-9xl font-extrabold text-stroke text-center">
+          Portfolio
+        </h1>
+        <div className="mt-4 flex flex-wrap justify-center">
+          {portfolioItems.map(({ node: item }) => (
+            <div key={item.id} className="w-full md:w-1/2 lg:w-1/3 p-1">
+              <Img
+                className="overflow-hidden rounded-lg"
+                fluid={item.image.fluid}
+                alt={item.title}
+              />
+              <h3
+                className="mt-2 text-4xl font-bold font-black leading-none text-accents-900"
+                style={{ color: item.color }}
+              >
+                {item.name}
+              </h3>
+              <span className="inline-block mt-2 rounded-full px-2 bg-accents-700 text-accents-100">
+                {item.category}
               </span>
-            </span>
-            ,
-            {' '}
-            <span>
-              #lit
-              <span role="img" aria-label="Fire">
-                🔥
-              </span>
-            </span>
-          </p>
-          <div className="hv-section-portfolio hv-mt-3">
-            {portfolioItems.map(({ node: item }) => (
-              <div className="columns hv-section-portfolio-item">
-                <div className="column is-4">
-                  <Img fluid={item.image.fluid} alt={item.title} />
-                </div>
-                <div className="column is-8 content">
-                  <h4
-                    className="title hv-portfolio-title is-1"
-                    style={{ color: item.color }}
-                  >
-                    {item.name}
-                  </h4>
-                  <p className="hv-portfolio-desc">
-                    {item.description.description}
-                  </p>
-                  <a
-                    href={item.link}
-                    style={{ backgroundColor: item.color }}
-                    className="button is-fullwidth is-rounded is-primary"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    {item.linkTitle}
-                  </a>
-                </div>
-              </div>
-            ))}
-          </div>
+              <p className="mt-4 text-accents-700 leading-relaxed ">
+                {item.description.description}
+              </p>
+              <a
+                className="block bg-accents-100 shadow-md mt-4 px-4 py-2 text-center rounded transform transition-all ease-in-out duration-200 hover:translate-y-1"
+                href={item.link}
+              >
+                See it
+              </a>
+            </div>
+          ))}
         </div>
-      </section>
-      <My />
+      </div>
     </Layout>
   );
 };
@@ -84,7 +62,6 @@ export const query = graphql`
           id
           name
           slug
-          color
           description {
             description
           }
@@ -95,7 +72,6 @@ export const query = graphql`
           }
           category
           link
-          linkTitle
         }
       }
     }
