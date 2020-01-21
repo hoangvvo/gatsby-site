d033bb07ba08905920e49c711b848eaf83acf463
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-01-20 19:49:32 -0500

    Open portfolio item in new tab

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -39,6 +39,8 @@ export default function PortfolioPage({ data }) {
                 {item.description.description}
               </p>
               <a
+                target="_blank"
+                rel="noopener noreferrer"
                 className="block bg-accents-100 shadow-md mt-4 px-4 py-2 text-center rounded transform transition-all ease-in-out duration-200 hover:translate-y-1"
                 href={item.link}
               >
