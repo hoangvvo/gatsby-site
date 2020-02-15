a971ba76bb1977872250611f8ecb928e137e0485
Hoang <40987398+hoangvvo@users.noreply.github.com>
2020-02-15 03:39:44 -0500

    Style Portfolio page (#41)
    * Make image optional
    
    * Add date
    
    * Add tags
    
    * change default image

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -20,24 +20,48 @@ export default function PortfolioPage({ data }) {
         </h1>
         <div className="mt-4 flex flex-wrap justify-center">
           {portfolioItems.map(({ node: item }) => (
-            <div key={item.id} className="w-full md:w-1/2 lg:w-1/3 p-1">
-              <Img
-                className="overflow-hidden rounded-lg"
-                fluid={item.image.fluid}
-                alt={item.title}
-              />
-              <h3
-                className="mt-2 text-4xl font-bold font-black leading-none text-accents-900"
-                style={{ color: item.color }}
-              >
-                {item.name}
-              </h3>
-              <span className="inline-block mt-2 rounded-full px-2 bg-accents-700 text-accents-100">
-                {item.category}
-              </span>
-              <p className="mt-4 text-accents-700 leading-relaxed ">
+            <div key={item.id} className="w-full md:w-1/2 lg:w-1/3 p-4">
+              {item.image ? (
+                <Img
+                  className="overflow-hidden rounded-lg"
+                  fluid={item.image.fluid}
+                  alt={item.title}
+                />
+              ) : (
+                <img
+                  alt="No logo"
+                  src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+                  className="theme-gradient overflow rounded-lg w-full"
+                  style={{ filter: 'grayscale(1) brightness(0.5)' }}
+                />
+              )}
+              <div className="mt-2">
+                <span className="inline-block align-middle bg-accents-700 text-accents-100 rounded-full px-2">
+                  {item.category}
+                </span>{' '}
+                <h3
+                  className="inline align-middle text-3xl font-bold font-black leading-none text-accents-900"
+                  style={{ color: item.color }}
+                >
+                  {item.name}
+                </h3>{' '}
+              </div>
+              <p className="mt-1 align-middle text-accents-600 text-sm">
+                {item.startDate} - {item.isOngoing ? 'present' : item.endDate}
+              </p>
+              <p className="text-accents-700 leading-relaxed ">
                 {item.description.description}
               </p>
+              <ul>
+                {item.tags.map(tag => (
+                  <li
+                    key={`tag:${tag}`}
+                    className="font-mono inline-block underline-bg theme-gradient opacity-75 text-xs mr-1"
+                  >
+                    {tag}
+                  </li>
+                ))}
+              </ul>
               <a
                 target="_blank"
                 rel="noopener noreferrer"
@@ -56,11 +80,14 @@ export default function PortfolioPage({ data }) {
 
 export const query = graphql`
   query PortfolioQuery {
-    allContentfulPortfolio {
+    allContentfulPortfolio(sort: { fields: [startDate], order: DESC }) {
       edges {
         node {
           id
           name
+          isOngoing
+          startDate(formatString: "MMM YYYY")
+          endDate(formatString: "MMM YYYY")
           description {
             description
           }
@@ -70,6 +97,7 @@ export const query = graphql`
             }
           }
           category
+          tags
           link
         }
       }
