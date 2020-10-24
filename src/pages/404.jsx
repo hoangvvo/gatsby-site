2a28460d178455254dc8605ac2450f4e783c030b
Hoang Vo <g@hvvo.dev>
2020-10-24 13:57:35 -0400

    haha not funny

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -10,7 +10,7 @@ export default function NotFoundPage() {
       <div className="w-full text-center px-2 py-64 flex flex-col items-center justify-center">
         <h1 className="text-6xl font-bold uppercase">Not found</h1>
         <p className="py-1 text-lg text-accents-600 mb-8">
-          This page does not exist... just like my girlfriend.
+          This page does not exist.
         </p>
         <Link
           className="transition-all ease-in-out duration-200 opacity-75 bg-accents-100 transform hover:translate-y-1 font-bold py-2 px-4 shadow-md rounded"
